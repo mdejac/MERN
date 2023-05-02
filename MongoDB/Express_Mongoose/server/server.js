9cdd26124b0b62c780621d49a839bf6dbbc0ccc5
mdejac <dejacm@gmail.com>
2023-05-02 18:55:56 -0400

    Express_Mongoose commit

---
+++ MongoDB/Express_Mongoose/server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+require("./config/mongoose.config");
+    
+app.use(express.json(), express.urlencoded({ extended: true }));
+    
+const AllMyUserRoutes = require("./routes/user.routes");
+AllMyUserRoutes(app);
+
+
+
+app.listen(port, () => console.log(`Listening on port: ${port}`) );
