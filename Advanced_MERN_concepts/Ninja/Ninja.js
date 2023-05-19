108ebd1ed6b294553bb82c4185540d8a2cfdcc9a
mdejac <dejacm@gmail.com>
2023-05-18 21:16:30 -0400

    Ninja commit

---
+++ Advanced_MERN_concepts/Ninja/Ninja.js
@@ -0,0 +1,31 @@
+class Ninja {
+    constructor(name, health=100, speed=3, strength=3) {
+        this.name = name;
+        this.health = health;
+        this.speed = speed;
+        this.strength = strength;
+    }
+
+    sayName() {
+        console.log(`Hello, My name is ${this.name}.`);
+        return this
+    }
+
+    showStats() {
+        console.log('Name : ', this.name);
+        console.log('Health :', this.health);
+        console.log('Speed : ', this.speed);
+        console.log('Strength : ', this.strength);
+        return this
+    }
+
+    drinkSake() {
+        this.health += 10;
+        return this
+    }
+}
+
+
+const hiro = new Ninja('Hiro');
+
+hiro.sayName().showStats().drinkSake().showStats();
