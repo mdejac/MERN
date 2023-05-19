dbba58f8e28630ea34e9c35e08308ed78a10eb76
mdejac <dejacm@gmail.com>
2023-05-18 23:23:09 -0400

    Time_to_Duel commit

---
+++ Advanced_MERN_concepts/Time_to_Duel/Deck.js
@@ -0,0 +1,57 @@
+class Card {
+    constructor (name, cost){
+        this.name = name;
+        this.cost = cost;
+    }
+}
+
+class Unit extends Card {
+    constructor (name, cost, power, res) {
+        super (name, cost);
+        this.power = power;
+        this.res = res;
+    }
+
+    attack(target) {
+        target.res -= this.power;
+        return this
+    }
+
+    display() {
+        console.log('Name :', this.name);
+        console.log('Power :', this.power);
+        console.log('Resilience :', this.res);
+        return this
+    }
+}
+
+class Effect extends Card {
+    constructor(name, cost, text, stat, magnitude) {
+        super(name, cost);
+        this.text = text;
+        this.stat = stat;
+        this.magnitude = magnitude;
+    }
+
+    play(target) {
+        if (target instanceof Unit) {
+            target[this.stat] += this.magnitude;
+        } else {
+            throw new Error('Target must be a unit.');
+        }
+        return this
+    }
+}
+
+const unit1 = new Unit('Red Belt Ninja', 3, 3, 4);
+const effect1 = new Effect('Hard Algorithm', 2, "Increase target's resilience by 3", 'res', 3);
+effect1.play(unit1);
+const unit2 = new Unit('Black Belt Ninja', 4, 5, 4);
+const effect2 = new Effect('Unhandled Promise Rejection', 1, "Decrease target's resilience by 1", 'res', -1);
+effect2.play(unit1);
+const effect3 = new Effect('Pair Programming', 3, "Increase target's power by 2", 'power', 2);
+effect3.play(unit1);
+unit1.attack(unit2);
+
+unit1.display();
+unit2.display();
