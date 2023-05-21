becf211ee8ee34e1d67c6941f474d78fe78929d7
mdejac <dejacm@gmail.com>
2023-05-20 23:10:00 -0400

    The_Handshake commit

---
+++ Socket_IO/The_Handshake/client/src/components/Chat.jsx
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react';
+import io from 'socket.io-client'
+
+const Chat = () => {
+    const [socket] = useState(()=>io(':8000'));
+
+    useEffect(() => {
+        console.log('Is this running?');
+        socket.on('Welcome', data => console.log(data));
+        return () => socket.off("Welcome");
+    }, [socket]);
+
+    return (
+        <div>
+            <h1>Socket Test</h1>
+        </div>
+    )
+}
+
+export default Chat
---
+++ Socket_IO/The_Handshake/server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const socket = require('socket.io');
+const port = 8000;
+app.use(cors());
+ 
+const server = app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+});
+ 
+const io = socket(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['*'],
+        credentials: true,
+    }
+});
+
+io.on("connection", socket => {
+    console.log('Nice to meet you (shake hand)');
+    socket.emit('Welcome', "WELCOME TO THE SERVER!");
+});
+
+
+
